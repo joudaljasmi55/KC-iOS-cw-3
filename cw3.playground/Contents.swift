d50fcd232aa01370dbe545396883627c7e4793d5
joudaljasmi55 <joudaljasmi@gmail.com>
2022-08-03 19:25:42 +0300

    done cw

---
+++ cw3.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var favouriteNumber: Int = 15
+var name:String = "joud"
+var hobby = "sleeping"
+
+let BirthOfDate = 2007
+
+print("hello my name is \(name) and my favourite number is \(favouriteNumber) and my hobby is \(hobby). \n And i was born in \(BirthOfDate)")
